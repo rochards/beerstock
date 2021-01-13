decb55ee3a9f664b670ace08bba5dc353be75ebe
Rodrigo <rodrigorochasantos@outlook.com>
2021-01-13 18:56:57 -0300

    Overridden handleMethodArgumentNotValid on APIExceptionHandler.

--- src/main/java/com/rochards/beerstock/exception/APIExceptionHandler.java
+++ src/main/java/com/rochards/beerstock/exception/APIExceptionHandler.java
@@ -5,14 +5,20 @@ import com.rochards.beerstock.exception.type.BeerNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
-public class APIExceptionHandler {
+public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BeerAlreadyExistException.class)
     public ResponseEntity<Object> handleBeerAlreadyExistException(BeerAlreadyExistException ex, WebRequest request) {
@@ -32,5 +38,23 @@ public class APIExceptionHandler {
         APIError apiError = new APIError(OffsetDateTime.now(), status.value(), error);
 
         return new ResponseEntity<Object>(apiError, new HttpHeaders(), status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+        HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+
+        var message = "One or more fields are invalid!";
+        APIError apiError = new APIError(OffsetDateTime.now(), status.value(), message, errors);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
     }
 }
