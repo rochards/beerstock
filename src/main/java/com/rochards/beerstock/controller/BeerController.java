6774020aed6d60ea55fc1b1a5bd381e2e9feea60
Rodrigo <rodrigorochasantos@outlook.com>
2021-01-12 21:19:51 -0300

    Changed response type on create and listAll methods on BeerController

--- src/main/java/com/rochards/beerstock/controller/BeerController.java
+++ src/main/java/com/rochards/beerstock/controller/BeerController.java
@@ -1,10 +1,12 @@
 package com.rochards.beerstock.controller;
 
 import com.rochards.beerstock.dto.BeerDTO;
+import com.rochards.beerstock.entity.Beer;
 import com.rochards.beerstock.service.BeerService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,13 +20,14 @@ public class BeerController {
     private final BeerService beerService;
 
     @GetMapping
-    public List<BeerDTO> listAll() {
-        return beerService.listAll();
+    public ResponseEntity<List<BeerDTO>> listAll() {
+        List<BeerDTO> beers = beerService.listAll();
+        return ResponseEntity.ok(beers);
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public BeerDTO create(@Valid @RequestBody BeerDTO beerDTO) {
-        return beerService.create(beerDTO);
+    public ResponseEntity<BeerDTO> create(@Valid @RequestBody BeerDTO beerDTO) {
+        BeerDTO createdBeer = beerService.create(beerDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdBeer);
     }
 }
